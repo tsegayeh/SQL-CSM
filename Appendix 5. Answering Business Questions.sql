a26fd492c1eae7f7883bfdf286eeeb82fb35c14c
tsegayeh <52754372+tsegayeh@users.noreply.github.com>
2021-03-04 14:06:19 -0800

    Update Appendix 5. Answering Business Questions.sql

--- Appendix 5. Answering Business Questions.sql
+++ Appendix 5. Answering Business Questions.sql
@@ -1,29 +1,27 @@
--------------------------------------------------------------------------------------------------------------
---			   Answering Business Questions																--
---				 Summary of T-SQL Queries																--
-			  															 
--------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+--		   Answering Business Questions																--
+--		     Summary of T-SQL Queries																--		  															 
+------------------------------------------------------------------------------------------
 
 USE [CSMdb]
 GO
-
---	a) What is the status of the sponsored child’s health and education? -------------
+--	a) What is the status of the sponsored child’s health and education?  
 --01. Child Status											
 	Execute [dbo].[sp_01GetChildStatus] 2 -- (e.g. for Child ID = 2)
 
 --02. Children Status
 	Execute [dbo].[sp_02GetChildrenStatus] -- (for all Children)
 	
----------------------------------------------------------------------
---	b) Who are your sponsors/donors? What do they donate and how much? --------------
+---------------------------------------------------------------------------
+--	b) Who are your sponsors/donors? What do they donate and how much?  
 --03. Funding: Number of Donors and Donations
 Execute [dbo].[sp_03GetNumDonorsAndDonations]
 
 --04a. Funding: Receipts total
 Execute [dbo].[sp_04A_GetTotalReceipts]
 
-------------------------------------------------------------- 
---	c) How is the organization’s performance in fundraising? -----------------------
+---------------------------------------------------------------- 
+--	c) How is the organization’s performance in fundraising? 
 --04b. Funding: Pledges total
 Execute [dbo].[sp_04B_GetTotalPledges]
 
@@ -49,15 +47,16 @@ Execute [dbo].[sp_10GetPledgesByIndivInKindDonor] 3 --(for Donor ID 2)
 --11. Funding: Pledges, In-Kind Grants, all donors
 Execute [dbo].[sp_11GetPledgesAllInKIndDonors]
 
----------------------------------------------------
---	d) What are the program goals and activities? ------------------------------------
+-----------------------------------------------------
+--	d) What are the program goals and activities?  
 --12. Program Goals 
 Execute [dbo].[sp_12GetGoals] 
 
 --13. Program Activities: Major activities
 Execute [dbo].[sp_13GetMajorActivities]
 
---	e) How effective and efficient is the utilization of the funds? ------------------
+-----------------------------------------------------------------------
+--	e) How effective and efficient is the utilization of the funds?  
 --14. Program Budget: Total
 Execute [dbo].[sp_14GetTotalBudget]
 
